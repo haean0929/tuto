827ec982abc8768ccf8a42baf6638d2bc39cac49
전수연 <haean0929@hanmail.net>
2018-03-08 20:44:35 +0900

    ExceptionExamples

---
+++ ExceptionExamples/src/NullPointExam.java
@@ -0,0 +1,17 @@
+
+public class NullPointExam {
+
+	public static void main(String[] args) {
+		try{
+			String str = null;
+		System.out.println("문자열: " + str.length());
+		}
+	catch(NullPointerException npe) {
+		System.out.println("npe.tiString():"+npe.toString());
+		}finally {
+			System.out.println("무조건 실행");
+	}
+
+	}
+
+}
